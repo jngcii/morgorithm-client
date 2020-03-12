f48f11ed501347270406e601a10340bd60c4e981
jngcii <concotree@gmail.com>
2020-03-13 02:52:28 +0900

    Set styles config

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import classNames from 'classnames/bind';
 import styles from "./styles.module.scss";
+const cx = classNames.bind(styles);
 
 function App() {
+  const isit = false;
+
   return (
-    <div className={styles.abc}>
-      Hello
+    <div className={cx('test', {red: isit})}>
+      morgorithm
     </div>
   );
 }
