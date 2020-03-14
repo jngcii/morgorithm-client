1c2467c055e4e817b4801399eb374fb1c0a8e076
jngcii <concotree@gmail.com>
2020-03-15 02:06:53 +0900

    Add navlist component

--- src/Components/Nav/index.js
+++ src/Components/Nav/index.js
@@ -2,9 +2,10 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 import Auth from "../Auth";
+import NavList from "../NavList";
 const cx = classNames.bind(styles);
 
-export default ({isLoggedIn, setIsLoggedIn}) => (
+export default ({ isLoggedIn, setIsLoggedIn }) => (
   <nav className={cx(isLoggedIn ? "in" : "out")}>
     <div className={cx("logoContainer")}>
       <span className={cx(isLoggedIn ? "in" : "out")}>morgorithm</span>
@@ -14,7 +15,7 @@ export default ({isLoggedIn, setIsLoggedIn}) => (
     <div className={cx(isLoggedIn ? "fakein" : "fakeout")} />
 
     <div className={cx("right")}>
-      {!isLoggedIn && <Auth setIsLoggedIn={setIsLoggedIn} />}
+      {isLoggedIn ? <NavList /> : <Auth setIsLoggedIn={setIsLoggedIn} />}
     </div>
   </nav>
 );
