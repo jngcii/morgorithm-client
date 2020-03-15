6289a21abb22de533fad1a22ab485faedfe00fba
jngcii <concotree@gmail.com>
2020-03-15 19:19:56 +0900

    Change bgColor from ##f5f5f5 to #f1f1f1

--- src/Screens/MainScreen/index.js
+++ src/Screens/MainScreen/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import SectionUser from "../../Components/SectionUser";
 export default () => (
-  <div style={{backgroundColor:"#fff"}}>
+  <div style={{backgroundColor:"#f2f2f2"}}>
     <SectionUser />
 
     <div style={{height: 500}} />
