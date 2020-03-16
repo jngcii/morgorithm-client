fe37345c4e256c707fcde27debad7caa33c3fb29
jngcii <concotree@gmail.com>
2020-03-16 12:47:09 +0900

    Add problem section component of main screen

--- src/Screens/MainScreen/index.js
+++ src/Screens/MainScreen/index.js
@@ -2,13 +2,13 @@ import React from "react";
 import SectionUser from "../../Components/SectionUser";
 import SolvingStatusBar from "../../Components/SolvingStatusBar";
 import SectionSolvingList from "../../Components/SectionSolvingList";
+import SectionProblem from "../../Components/SectionProblem";
 
 export default () => (
   <div style={{backgroundColor:"#f2f2f2"}}>
     <SectionUser />
     <SolvingStatusBar />
     <SectionSolvingList />
-
-    <div style={{height: 500}} />
+    <SectionProblem />
   </div>
 );
