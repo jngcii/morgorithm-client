bf487aa8a56ff78e2f8fcf86b3135e4d104ba8cf
jngcii <concotree@gmail.com>
2020-03-20 02:34:01 +0900

    Apply router to line problem

--- src/Components/LineProblem/index.js
+++ src/Components/LineProblem/index.js
@@ -1,16 +1,19 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 const cx = classNames.bind(styles);
 
-export default ({site, num, title, level, solved}) => (
+export default ({ site, num, title, level, solved }) => (
   <div className={cx("wrapper")}>
-    <div className={cx("container")}>
-      <div className={cx("num")}>{num}</div>
-      <div className={cx("site")}>{site}</div>
-      <div className={cx("title")}>{title}</div>
-      <div className={cx(level ? "level" : "noLevel")}>{level}</div>
-      <div className={cx("solved")} />
-    </div>
+    <Link className={cx("link")} to={`/problem/${1234}`}>
+      <div className={cx("container")}>
+        <div className={cx("num")}>{num}</div>
+        <div className={cx("site")}>{site}</div>
+        <div className={cx("title")}>{title}</div>
+        <div className={cx(level ? "level" : "noLevel")}>{level}</div>
+        <div className={cx("solved")} />
+      </div>
+    </Link>
   </div>
 );
