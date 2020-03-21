a79ba97317fd7b1c3b48420b6a0ea5b905e753bc
jngcii <concotree@gmail.com>
2020-03-22 02:06:21 +0900

    Update solution line component

--- src/Components/LineSolution/index.js
+++ src/Components/LineSolution/index.js
@@ -1,10 +1,28 @@
 import React from "react";
-import classNames from "classnames";
+import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
+import User from "../User";
 const cx = classNames.bind(styles);
 
 export default () => (
-  <div>
-    
+  <div className={cx("wrapper")}>
+    <div className={cx("user")}>
+      <User />
+    </div>
+
+    <div className={cx("cnt")}>
+      <img src={require("../../assets/grey-small-view.png")} />
+      <span className={cx("num")}>298</span>
+    </div>
+
+    <div className={cx("cnt")}>
+      <img src={require("../../assets/grey-small-like.png")} />
+      <span className={cx("num")}>15</span>
+    </div>
+
+    <div className={cx("cnt")}>
+      <img src={require("../../assets/grey-small-comment.png")} />
+      <span className={cx("num")}>113</span>
+    </div>
   </div>
 );
