b2626663170ded313a26276cc72f8a48fb23bfe8
jngcii <concotree@gmail.com>
2020-03-21 17:37:31 +0900

    Add line solution files

---
+++ src/Components/LineSolution/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import classNames from "classnames";
+import styles from "./styles.module.scss";
+const cx = classNames.bind(styles);
+
+export default () => (
+  <div>
+    
+  </div>
+);
