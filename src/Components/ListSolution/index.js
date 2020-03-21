a95cfe12be6da8ce3be5dde795bd8d4926c69b14
jngcii <concotree@gmail.com>
2020-03-22 02:07:57 +0900

    Update solution/question list component

--- src/Components/ListSolution/index.js
+++ src/Components/ListSolution/index.js
@@ -1,15 +1,28 @@
 import React from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
+import LineSolution from "../LineSolution";
+import LineQuestion from "../LineQuestion";
 const cx = classNames.bind(styles);
 
-export default ({ subject = "다른 사람의 풀이" }) => (
+export default ({ subject }) => (
   <div className={cx("wrapper")}>
     <header>
-      <h4>{subject}</h4>
+      {subject === "solution" && <h4>다른 사람의 풀이</h4>}
+      {subject === "question" && <h4>질문 목록</h4>}
+      <div class={cx("more")}>더보기</div>
     </header>
 
     <section>
+      <div className={cx("line")}>
+        {subject === "solution" && <LineSolution />}
+        {subject === "question" && <LineQuestion />}
+      </div>
+
+      <div className={cx("line")}>
+        {subject === "solution" && <LineSolution />}
+        {subject === "question" && <LineQuestion />}
+      </div>
     </section>
   </div>
 );
