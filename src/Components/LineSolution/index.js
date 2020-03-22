fa6a0958a549dccef5bac0ac62113e198e8b57db
jngcii <concotree@gmail.com>
2020-03-22 15:48:36 +0900

    Make cnt elements to distinct component

--- src/Components/LineSolution/index.js
+++ src/Components/LineSolution/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 import User from "../User";
+import Cnt from "../Cnt";
 const cx = classNames.bind(styles);
 
 export default () => (
@@ -10,19 +11,6 @@ export default () => (
       <User />
     </div>
 
-    <div className={cx("cnt")}>
-      <img src={require("../../assets/grey-small-view.png")} />
-      <span className={cx("num")}>298</span>
-    </div>
-
-    <div className={cx("cnt")}>
-      <img src={require("../../assets/grey-small-like.png")} />
-      <span className={cx("num")}>15</span>
-    </div>
-
-    <div className={cx("cnt")}>
-      <img src={require("../../assets/grey-small-comment.png")} />
-      <span className={cx("num")}>113</span>
-    </div>
+    <Cnt />
   </div>
 );
