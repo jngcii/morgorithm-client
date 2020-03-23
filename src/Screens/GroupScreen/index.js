472d4fa254318e8809174837235687124a8eee43
jngcii <concotree@gmail.com>
2020-03-23 18:48:15 +0900

    Add group screen

---
+++ src/Screens/GroupScreen/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import classNames from "classnames/bind";
+import styles from "./styles.module.scss";
+import ProfileOverview from "../../Components/ProfileOverview";
+import Group from "../../Components/Group";
+const cx = classNames.bind(styles);
+
+export default () => (
+  <div className={cx("wrapper")}>
+    <ProfileOverview />
+    <Group />
+  </div>
+)
