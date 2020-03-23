1150152ad623109a98c987d24b874409144e9020
jngcii <concotree@gmail.com>
2020-03-24 00:20:28 +0900

    Add group search list component

--- src/Components/ListGroupSearch/index.js
+++ src/Components/ListGroupSearch/index.js
@@ -1,41 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 const cx = classNames.bind(styles);
 
-export default () => (
-  <div className={cx("wrapper")}>
-    <table>
-      <thead>
-        <tr>
-          <th className={cx("name")}>그룹명</th>
-          <th className={cx("cnt")}>인원</th>
-          <th className={cx("access")}></th>
-          <th className={cx("btn")}></th>
-        </tr>
-      </thead>
+export default ({ gname }) => {
+  const [choosen, setChoosen] = useState(null);
+  const [password, setPassword] = useState("");
 
-      <tbody>
-        <tr>
-          <th className={cx("name")}>ssafy31</th>
-          <th className={cx("cnt")}>26</th>
-          <th className={cx("access")}>lock</th>
-          <th className={cx("btn")}>leave</th>
-        </tr>
-        <tr>ghalksdghalskdg</tr>
-        <tr>
-          <th className={cx("name")}>ssafy31</th>
-          <th className={cx("cnt")}>26</th>
-          <th className={cx("access")}>lock</th>
-          <th className={cx("btn")}>leave</th>
-        </tr>
-        <tr>
-          <th className={cx("name")}>ssafy31</th>
-          <th className={cx("cnt")}>26</th>
-          <th className={cx("access")}>lock</th>
-          <th className={cx("btn")}>leave</th>
-        </tr>
-      </tbody>
-    </table>
-  </div>
-)
+  return (
+    <div className={cx("wrapper")}>
+      <table>
+        <thead>
+          <tr>
+            <th className={cx("name")}>그룹명</th>
+            <th className={cx("cnt")}>인원</th>
+            <th className={cx("access")}>공개</th>
+            <th className={cx("btn")}></th>
+          </tr>
+        </thead>
+
+        {gname ? (
+          <tbody>
+            {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13].map(i => (
+              <div key={i} className={cx("line")}>
+                <tr>
+                  <th className={cx("name")}>ssafy31</th>
+                  <th className={cx("cnt")}>26</th>
+                  <th className={cx("access")}>
+                    <img src={require("../../assets/lock.png")} />
+                  </th>
+                  <th className={cx("btn")}>
+                    <button onClick={() => setChoosen(i)}>enter</button>
+                  </th>
+                </tr>
+
+                <input
+                  className={cx("pwInput", choosen !== i && "non")}
+                  placeholder={"비밀번호를 입력하세요"}
+                  value={password}
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </div>
+            ))}
+          </tbody>
+        ) : (
+          <div className={cx("box")}>
+            <img src={require("../../assets/search.png")} />
+            <div>search</div>
+          </div>
+        )}
+      </table>
+    </div>
+  );
+};
