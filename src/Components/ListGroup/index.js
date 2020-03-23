9c55d3c118292bfbabb803d77b7ba7e04a7680f9
jngcii <concotree@gmail.com>
2020-03-24 00:20:07 +0900

    Add group list component

--- src/Components/ListGroup/index.js
+++ src/Components/ListGroup/index.js
@@ -3,11 +3,11 @@ import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
 const cx = classNames.bind(styles);
 
-export default () => (
+export default ({ searching }) => (
   <div className={cx("wrapper")}>
     <table>
       <thead>
-        <tr>
+        <tr className={cx(!searching && "non")}>
           <th className={cx("name")}>그룹명</th>
           <th className={cx("cnt")}>인원</th>
           <th className={cx("btn")}></th>
@@ -15,22 +15,28 @@ export default () => (
       </thead>
 
       <tbody>
-        <tr>
+        <tr className={cx(!searching && "non")}>
           <th className={cx("name")}>ssafy31</th>
           <th className={cx("cnt")}>26</th>
-          <th className={cx("btn")}>leave</th>
+          <th className={cx("btn")}>
+            <button>leave</button>
+          </th>
         </tr>
-        <tr>
-          <th className={cx("name")}>ssafy31</th>
-          <th className={cx("cnt")}>26</th>
-          <th className={cx("btn")}>leave</th>
+        <tr className={cx(!searching && "non")}>
+          <th className={cx("name")}>my group</th>
+          <th className={cx("cnt")}>7</th>
+          <th className={cx("btn")}>
+            <button>leave</button>
+          </th>
         </tr>
-        <tr>
-          <th className={cx("name")}>ssafy31</th>
-          <th className={cx("cnt")}>26</th>
-          <th className={cx("btn")}>leave</th>
+        <tr className={cx(!searching && "non")}>
+          <th className={cx("name")}>your group</th>
+          <th className={cx("cnt")}>12</th>
+          <th className={cx("btn")}>
+            <button>leave</button>
+          </th>
         </tr>
       </tbody>
     </table>
   </div>
-)
+);
