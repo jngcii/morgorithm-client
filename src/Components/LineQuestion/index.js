cedebaf68b4ea1bc69ea306057a7f8b57939e9a0
jngcii <concotree@gmail.com>
2020-03-25 03:13:07 +0900

    Add sub line in quesiton line component

--- src/Components/LineQuestion/index.js
+++ src/Components/LineQuestion/index.js
@@ -1,23 +1,31 @@
 import React from "react";
 import classNames from "classnames/bind";
 import styles from "./styles.module.scss";
-import User from "../User";
 const cx = classNames.bind(styles);
 
-export default () => (
-  <div className={cx("wrapper")}>
-    <div className={cx("title")}>
-      어디가 틀린지 모르겠어요 ㅠㅠ
-    </div>
+export default ({ isDetail }) => (
+  <div>
+    {!!isDetail && (
+      <div className={cx("prob")}>
+        <span className={cx("span")}>Baekjoon</span>
+        <span className={cx("span")}>1234</span>
+        <span className={cx("span")}>악어새를 찾아라!</span>
+        <span className={cx("span")}>D3</span>
+      </div>
+    )}
 
-    <div className={cx("cnt")}>
-      <img src={require("../../assets/grey-small-view.png")} />
-      <span className={cx("num")}>298</span>
-    </div>
+    <div className={cx("wrapper")}>
+      <div className={cx("title")}>어디가 틀린지 모르겠어요 ㅠㅠ</div>
 
-    <div className={cx("cnt")}>
-      <img src={require("../../assets/grey-small-comment.png")} />
-      <span className={cx("num")}>113</span>
+      <div className={cx("cnt")}>
+        <img src={require("../../assets/grey-small-view.png")} />
+        <span className={cx("num")}>298</span>
+      </div>
+
+      <div className={cx("cnt")}>
+        <img src={require("../../assets/grey-small-comment.png")} />
+        <span className={cx("num")}>113</span>
+      </div>
     </div>
   </div>
 );
