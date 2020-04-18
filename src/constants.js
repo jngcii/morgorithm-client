8e4aced2a9369b444e2aadea350683c1853c6c21
jngcii <concotree@gmail.com>
2020-04-18 23:10:03 +0900

    Update api url

--- src/constants.js
+++ src/constants.js
@@ -1,3 +1,3 @@
-export const API_URL = "https://api.helpmydirtycode.com/api";
+export const API_URL = "https://morgoapiserver.com/api";
 export const MEDIA_URL = "";
 export const GOOGLE_CLIENT_ID = "862350269939-9ratnq44ma0a2590peil500mpjsd5evm.apps.googleusercontent.com";
